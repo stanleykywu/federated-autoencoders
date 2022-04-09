3cbfb67529c1d95c907a766ac2fcd2b3492308d5
stanleykywu <stanleykywu@gmail.com>
2022-04-09 13:51:41 -0400

    Initial commit for fmnist_client runner

--- models/fmnist_vae.py
+++ models/fmnist_vae.py
@@ -1,16 +1,10 @@
 import torch
 import torch.nn as nn
 
-import torchbearer
-from torchbearer import state_key
 
-MU = state_key("mu")
-LOGVAR = state_key("logvar")
-
-
-class VAE(nn.Module):
+class Fmnist_VAE(nn.Module):
     def __init__(self, latent_size):
-        super(VAE, self).__init__()
+        super(Fmnist_VAE, self).__init__()
         self.latent_size = latent_size
 
         self.encoder = nn.Sequential(
@@ -23,8 +17,8 @@ class VAE(nn.Module):
 
         self.mu = nn.Linear(64 * 7 * 7, latent_size)
         self.logvar = nn.Linear(64 * 7 * 7, latent_size)
-
         self.upsample = nn.Linear(latent_size, 64 * 7 * 7)
+
         self.decoder = nn.Sequential(
             nn.ConvTranspose2d(64, 32, 4, 2, 1),  # B,  64,  14,  14
             nn.ReLU(True),
@@ -33,27 +27,27 @@ class VAE(nn.Module):
             nn.ConvTranspose2d(32, 1, 4, 1, 2),  # B, 1, 28, 28
         )
 
-    def reparameterize(self, mu, logvar):
-        if self.training:
-            std = torch.exp(0.5 * logvar)
-            eps = torch.randn_like(std)
-            return eps.mul(std).add_(mu)
-        else:
-            return mu
+    def reparametrize(self, h):
+        """Reparametrization layer of VAE."""
+        mu, logvar = self.mu(h), self.logvar(h)
+        std = torch.exp(logvar / 2)
+        eps = torch.randn_like(std)
+        z = mu + std * eps
+        return z, mu, logvar
 
-    def forward(self, x, state):
-        image = x
-        x = self.encoder(x).relu().view(x.size(0), -1)
+    def encode(self, x):
+        """Encoder of the VAE."""
+        h = self.encoder(x)
+        z, mu, logvar = self.reparametrize(h)
+        return z, mu, logvar
 
-        mu = self.mu(x)
-        logvar = self.logvar(x)
-        sample = self.reparameterize(mu, logvar)
+    def decode(self, z):
+        """Decoder of the VAE."""
+        z = self.upsample(z)
+        z = self.decoder(z)
+        return z
 
-        result = self.decoder(self.upsample(sample).relu().view(-1, 64, 7, 7))
-
-        if state is not None:
-            state[torchbearer.Y_TRUE] = image
-            state[MU] = mu
-            state[LOGVAR] = logvar
-
-        return result
+    def forward(self, x):
+        z, mu, logvar = self.encode(x)
+        z_decode = self.decode(z)
+        return z_decode, mu, logvar
