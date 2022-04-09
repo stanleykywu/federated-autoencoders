65c31d329318230646b296eae4394520068a0cb9
stanleykywu <stanleykywu@gmail.com>
2022-04-09 14:27:26 -0400

    Stanford dataset

--- stanford_client.py
+++ stanford_client.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn.functional as F
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
-from torchvision.datasets import Food101
+from torchvision.datasets import StanfordCars
 
 import flwr as fl
 
@@ -18,11 +18,11 @@ def load_data():
         [
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-            transforms.Resize((3, 32, 32)),
+            transforms.Resize((32, 32)),
         ]
     )
-    trainset = Food101(".", train=True, download=True, transform=transform)
-    testset = Food101(".", train=False, download=True, transform=transform)
+    trainset = StanfordCars(".", split="train", download=True, transform=transform)
+    testset = StanfordCars(".", split="test", download=True, transform=transform)
     trainloader = DataLoader(trainset, batch_size=128, shuffle=True)
     testloader = DataLoader(testset, batch_size=128)
     return trainloader, testloader
