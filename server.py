e1aed8d8ec829882766524995660324557454940
Krish Sharma <krish.northeastern@gmail.com>
2022-04-10 11:34:29 -0400

    use single client

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 import flwr as fl
-
+from flwr.server.strategy import FedAvg, Strategy
 
 def main():
-    fl.server.start_server(config={"num_rounds": 3})
+    strategy = FedAvg(min_available_clients=1, min_fit_clients=1, min_eval_clients=1)
+    fl.server.start_server(config={"num_rounds": 1}, strategy=strategy)
 
 
 if __name__ == "__main__":
